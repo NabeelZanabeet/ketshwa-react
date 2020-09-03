d7311e143b76e18d3005f142ad899b33bcdd3ea0
NabeelZanabeet <nabeelzanabeet@gmail.com>
2020-09-03 04:14:18 +0300

    Added index file for components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
-import Users from './components/Users';
+import { Users } from './components';
 
 const client = new ApolloClient({
   uri: 'http://localhost:3000/graphql',
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -4,9 +4,9 @@ import './UserCard.css';
 function UserCard({ firstName, lastName, email }) {
   return (
     <div className="card-container">
-      <p>{firstName}</p>
-      <p>{lastName}</p>
-      <p>{email}</p>
+      <p className="firstName">{firstName}</p>
+      <p className="lastName">{lastName}</p>
+      <p className="email">{email}</p>
     </div>
   );
 }
