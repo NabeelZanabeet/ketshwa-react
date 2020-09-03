f535c34e9d1af8307f1f8d4ff9f4eae72599f5b4
NabeelZanabeet <nabeelzanabeet@gmail.com>
2020-09-03 04:59:14 +0300

    View users cards using a grid

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './UserCard.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-function UserCard({ firstName, lastName, email }) {
+const useStyles = makeStyles(() => ({
+  paper: {
+    padding: 10,
+    margin: 15,
+  },
+}));
+
+function UserCard({ id, firstName, lastName, email }) {
+  const classes = useStyles();
   return (
-    <div className="card-container">
-      <p className="firstName">{firstName}</p>
-      <p className="lastName">{lastName}</p>
-      <p className="email">{email}</p>
-    </div>
+    <Grid key={id} item>
+      <Paper className={classes.paper}>
+        <p className="firstName">{firstName}</p>
+        <p className="lastName">{lastName}</p>
+        <p className="email">{email}</p>
+      </Paper>
+    </Grid>
   );
 }
 
--- src/components/Users.js
+++ src/components/Users.js
@@ -4,36 +4,41 @@ import gql from 'graphql-tag';
 import UserCard from './UserCard';
 import './Users.css';
 import { loader } from '../assets';
+import Grid from '@material-ui/core/Grid';
 
 function Users() {
   return (
-    <div className="users-container">
-      <Query
-        query={gql`
-          {
-            users {
-              id
-              firstName
-              lastName
-              email
-            }
-          }
-        `}
-      >
-        {({ loading, error, data }) => {
-          if (loading) return <img alt="loader" src={loader} />;
-          if (error) return <p className="error-text">Error :(</p>;
-          return data.users.map(({ id, firstName, lastName, email }) => (
-            <UserCard
-              key={id}
-              email={email}
-              firstName={firstName}
-              lastName={lastName}
-            />
-          ));
-        }}
-      </Query>
-    </div>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Grid container justify="center" spacing={1}>
+          <Query
+            query={gql`
+              {
+                users {
+                  id
+                  firstName
+                  lastName
+                  email
+                }
+              }
+            `}
+          >
+            {({ loading, error, data }) => {
+              if (loading) return <img alt="loader" src={loader} />;
+              if (error) return <p className="error-text">Error :(</p>;
+              return data.users.map(({ id, firstName, lastName, email }) => (
+                <UserCard
+                  key={id}
+                  email={email}
+                  firstName={firstName}
+                  lastName={lastName}
+                />
+              ));
+            }}
+          </Query>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
